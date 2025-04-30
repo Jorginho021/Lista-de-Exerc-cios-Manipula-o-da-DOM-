59df5d81a50936d16eb0ae614d750da704cf2143
Jorge Cardoso de jesus <165197225+Jorginho021@users.noreply.github.com>
2025-04-30 14:50:24 -0300

    Update .js

--- .js
+++ .js
@@ -14,4 +14,27 @@ document.getElementById("botaoClique").addEventListener("click", function () {
     p.style.fontSize = "20px";
     p.style.backgroundColor = "yellow";
   });
+
+3:
+document.getElementById("adicionarEstilo").addEventListener("click", function () {
+    document.getElementById("paragrafo").classList.add("estiloAdicionado");
+  });
+  
+
+  document.getElementById("removerEstilo").addEventListener("click", function () {
+    document.getElementById("paragrafo").classList.remove("estiloAdicionado");
+  });
+  
+  document.getElementById("formSaudacao").addEventListener("submit", function (event) {
+    event.preventDefault(); 
+  
+   
+    const nome = document.getElementById("nome").value;
+  
+   
+    document.getElementById("saudacao").textContent = "Olá, " + nome + "!";
+  
+    
+    document.getElementById("nome").value = "";
+  });
   
